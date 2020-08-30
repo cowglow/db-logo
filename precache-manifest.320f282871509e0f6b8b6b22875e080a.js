9cc301df0e96da1263c0da90b77173b5fd896d00
Philip Saa <cowglow@gmail.com>
2020-08-30 20:53:03 +0200

    Updates

---
+++ precache-manifest.320f282871509e0f6b8b6b22875e080a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db5e278653f58d228f72c97d550ccf5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ad485d5e8d5e6afd351",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b32be9ab914e27d3e1fb",
+    "url": "/static/js/2.cdb8908a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cdb8908a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ad485d5e8d5e6afd351",
+    "url": "/static/js/main.f1c4c4a7.chunk.js"
+  },
+  {
+    "revision": "ab9ed91d1b09ea093edd",
+    "url": "/static/js/runtime-main.777a1685.js"
+  }
+]);
