b2ec4b2af7278ca202e787ebe317a66b4fe483ed
Philip Saa <cowglow@gmail.com>
2020-09-13 10:56:22 +0200

    pre-broadcast-check

---
+++ src/component/app/app.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+// import HocDbsLogo from "../hoc/hoc-dbs-logo";
+import styles from "./app.module.css"
+
+const App = () => (
+  <React.Fragment>
+    <div className={styles.root}>
+      working on it
+      {/* <HocDbsLogo /> */}
+    </div>
+  </React.Fragment>
+);
+
+export default App;
