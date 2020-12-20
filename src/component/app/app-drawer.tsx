16ba873974a55648128c6406f11baa033ef2c65d
Philip Saa <cowglow@gmail.com>
2020-12-20 22:08:27 +0100

    Seeds menu for app drawer.

--- src/component/app/app-drawer.tsx
+++ src/component/app/app-drawer.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 
 interface AppDrawerProps {
   open: boolean;
@@ -9,7 +12,24 @@ interface AppDrawerProps {
 const AppDrawer: React.FC<AppDrawerProps> = (props) => {
   return (
     <Drawer anchor="left" {...props}>
-      menu goes here
+      <div
+        role="presentation"
+        onClick={props.onClose}
+        onKeyDown={props.onClose}
+        style={{ width: 250 }}
+      >
+        <List>
+          <ListItem button>
+            <ListItemText primary={"Menu Item 1"} />
+          </ListItem>
+          <ListItem button>
+            <ListItemText primary={"Menu Item 2"} />
+          </ListItem>
+          <ListItem button>
+            <ListItemText primary={"Menu Item 3"} />
+          </ListItem>
+        </List>
+      </div>
     </Drawer>
   );
 };
